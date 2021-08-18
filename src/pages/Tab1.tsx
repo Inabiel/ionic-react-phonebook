b81028d2e4ad84c5df5451e3a381b952db5ae4f0
Inabiel <nabiel.p@students.amikom.ac.id>
2021-08-18 23:00:44 +0700

    initial commit

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,13 +1,43 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonList,
+  IonAvatar,
+  IonItem,
+  IonLabel,
+  IonIcon,
+} from "@ionic/react";
+import { mailOutline, briefcaseOutline } from "ionicons/icons";
+import Axios from "axios";
+import { useState, useEffect } from "react";
+import "./Tab1.css";
+import { Link } from "react-router-dom";
 
 const Tab1: React.FC = () => {
+  const [userProfile, setUserProfile] = useState<any>({});
+  const [isExists, setisExists] = useState(false);
+
+  const setUserProfileToState = async () => {
+    const data = await Axios.get(
+      "https://api.jsonbin.io/b/610d090de1b0604017a7a605"
+    );
+    const userResponse = data.data;
+    setUserProfile(userResponse);
+    setisExists(true);
+  };
+
+  useEffect(() => {
+    setUserProfileToState();
+  }, []);
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>My Phonebook</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
@@ -16,7 +46,39 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        {userProfile.length > 0 && (
+          <>
+            {userProfile.map((data: any) => (
+              <IonList key={userProfile.id}>
+                <Link
+                  to={`/detail/${data.id}`}
+                  style={{ textDecoration: "none" }}
+                >
+                  <IonItem>
+                    <IonAvatar>
+                      <img src={data.avatar} alt="" />
+                    </IonAvatar>
+                    <IonLabel className="ion-padding">
+                      <h3>
+                        {data.first_name} {data.last_name}{" "}
+                      </h3>
+                      <h3 className="ion-padding-top">
+                        <IonIcon icon={mailOutline}></IonIcon>
+                        <span className="ion-padding-start">{data.email}</span>
+                      </h3>
+                      <h3 className="ion-padding-top">
+                        <IonIcon icon={briefcaseOutline}></IonIcon>
+                        <span className="ion-padding-start">
+                          {data.company}
+                        </span>
+                      </h3>
+                    </IonLabel>
+                  </IonItem>
+                </Link>
+              </IonList>
+            ))}
+          </>
+        )}
       </IonContent>
     </IonPage>
   );
--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,22 +1,75 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab2.css';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonImg,
+  IonIcon,
+} from "@ionic/react";
+import Axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { mailOutline, briefcaseOutline } from "ionicons/icons";
+import "./Tab2.css";
 
 const Tab2: React.FC = () => {
+  const [userProfile, setUserProfile] = useState<any>({});
+  const { id }: any = useParams();
+
+  const setUserProfileToState = async () => {
+    const data = await Axios.get(
+      "https://api.jsonbin.io/b/610d090de1b0604017a7a605"
+    );
+    const userResponse = data.data.filter((item: any) => item.id == id);
+    setUserProfile(userResponse);
+  };
+
+  useEffect(() => {
+    setUserProfileToState();
+    console.log(id);
+  }, []);
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>Person</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
+            <IonTitle size="large">Detail Pegawai</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+        {userProfile.length > 0 && (
+          <>
+            <IonImg
+              style={{
+                width: "80px",
+                height: "80px",
+                margin: "0 auto",
+                marginTop: "40px",
+              }}
+              src={userProfile[0].avatar}
+            />
+            <h1 style={{ textAlign: "center" }}>
+              {userProfile[0].first_name} {userProfile[0].last_name}
+            </h1>
+            <h4 className="ion-text-center">
+              <IonIcon icon={mailOutline} style={{ marginRight: 10 }}></IonIcon>
+              {userProfile[0].email}
+            </h4>
+            <h4 className="ion-text-center">
+              <IonIcon
+                icon={briefcaseOutline}
+                style={{ marginRight: 10 }}
+              ></IonIcon>
+              {userProfile[0].company}
+            </h4>
+          </>
+        )}
       </IonContent>
     </IonPage>
   );
